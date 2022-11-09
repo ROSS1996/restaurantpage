04fc6f8991aafbe792e69605029868b93a9f3f98
Robson <41270745+ROSS1996@users.noreply.github.com>
2022-11-09 19:55:13 -0300

    Remove images

--- src/menu.js
+++ src/menu.js
@@ -11,31 +11,17 @@ const drinks = document.createElement('h3')
 drinks.innerText = 'Drinks'
 main.appendChild(drinks);
 
-import waterObj from './images/water.jpg';
-const waterImage = new Image();
-waterImage.src = waterObj;
-waterImage.classList.add('menuItem')
+
 const water = document.createElement('p')
 water.innerText = "Water: $1"
-main.appendChild(waterImage)
 main.appendChild(water);
 
-import sodaObj from './images/soda.jpg';
-const sodaImg = new Image();
-sodaImg.src = sodaObj;
-sodaImg.classList.add('menuItem')
 const soda = document.createElement('p')
 soda.innerText = "Soda: $2"
-main.appendChild(sodaImg)
 main.appendChild(soda);
 
-import beerObj from './images/beer.jpg';
-const beerImg = new Image();
-beerImg.src = beerObj;
-beerImg.classList.add('menuItem')
 const beer = document.createElement('p')
 beer.innerText = "Beer: $5"
-main.appendChild(beerImg)
 main.appendChild(beer);
 
 const foods = document.createElement('h3')
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'main.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
   module: {
@@ -12,18 +12,6 @@ module.exports = {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
-      {
-        test: /\.(gif|png|jpe?g|svg)$/i,
-        use: [
-          'file-loader',
-          {
-            loader: 'image-webpack-loader',
-            options: {
-              bypassOnDebug: true, // webpack@1.x
-              disable: true, // webpack@2.x and newer
-            },
-          },
-        ],
-      }]
+    ],
   },
 };
